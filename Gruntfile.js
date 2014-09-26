1c90c08fc95cbd82816ea8d6c61041d0bcfbd22a
Richard Healy <richard.healy@basekit.com>
2014-09-26 13:28:48 +0100

    Initial commit of the BKL

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  function recruseImports (filename) {
+  	var str = grunt.file.read(filename),
+  		filecontents = '',
+  		imports = /@import\W+?[\"\'](.*?)[\"\']\;/ig,
+  		matches = str.match(imports);
+
+  	if(matches !== null && matches.length > 0) {
+		for (var i = 0; i < matches.length; i++) {
+			swapOutString = matches[i];
+			filecontents = recruseImports(swapOutString.replace(imports, "$1"));
+			str = str.replace(matches[i], filecontents);
+			filecontents = '';
+		}
+	}
+	return str;
+  }
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    lesslint:{
+     src: ['bkl-dev.less']
+    },
+    csslint: {
+      options:{
+        csslintrc: 'config/.csslintrc'
+      }
+     }
+  });
+
+  grunt.loadNpmTasks('grunt-lesslint')
+
+  // Default task(s).
+  grunt.registerTask('build', function () { 	
+  	var buildStr = recruseImports('bk-less-framework.less'),
+        vars = grunt.file.read('vars.less');
+  	
+    // This is the version that gets linted; includes
+    // all the vars so linting doesn't fall over
+    grunt.file.write('bkl-dev.less', vars+buildStr);
+    
+    // This is the production version of bkb. The
+    // one that will get released with BaseKit
+    grunt.file.write('bkl.less', buildStr);
+        grunt.task.run('lesslint');
+  });
+};
+
---
+++ README.md
@@ -0,0 +1,46 @@
+bkbootstrap
+===========
+
+# New Version: BaseKit LESS Framework
+
+###Version 1.0
+
+Initial version: rework from original bkbootstrap setup
+
+===========
+===========
+===========
+
+# Old Version: BaseKit Boostrap
+
+#BaseKit Bootstrap File
+
+This gives each template some default styles to build upon. The files use LESS so simply add any additional styles into the appropriate classes.
+
+###Version 2.2
+
+Added more ecom widgets, menu and blog widgets. Fixes for various platforms. Compatible with 7.32
+
+###Version 2.1 
+
+Added ecom widget prototype. Compatible with BaseKit version 7.22
+
+###Version 2.0
+
+Breakdown of files to default and extended
+
+###Version 1.3
+
+Fixes for navigation widget
+
+###Version 1.2
+
+Added contact form overlay
+
+###Version 1.1
+
+Blog default css
+
+###Version 1.0
+
+Initial version
